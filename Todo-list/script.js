ba7ada626142a37fda410f4bfbe7b81055edd905
Sadie Graines <sadiegraines@Sadies-MacBook-Air.local>
2024-12-11 13:53:45 -0600

    todo list

---
+++ Todo-list/script.js
@@ -0,0 +1,32 @@
+
+
+let tasks = ["do homework" , "clean bathroom drawer"];
+function showTasks() {
+	let taskListUl = document.getElementById("task-list");
+	taskListUl.innerHTML = "";
+	
+	for(let i = 0; i < tasks.length; i++){
+		let li = document.createElement("li");
+		li.innerText = tasks[i];
+		taskListUl.appendChild(li);
+	}
+}
+
+function addTask() {
+	let task = prompt("Enter your task: ");
+	tasks.push(task);
+	showTasks();
+}
+
+function clearTasks() {
+	tasks = [];
+	showTasks();
+}
+
+
+function removeTask(){
+	let taskRemove = Number(prompt("Enter the task # you want to remove: "));
+	let taskRemove1 = taskRemove - 1;
+	tasks.splice(taskRemove1, 1);
+	showTasks();
+}
